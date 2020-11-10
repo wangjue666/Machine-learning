ea2bd93c202a0af03eff8011f468fbdc8342a07b
王珏 <wangjuesix@gmail.com>
2020-11-10 19:37:28 +0800

    计算KNN不同k紧邻度模型的准确率

--- sklearn-sd/trainKNN.py
+++ sklearn-sd/trainKNN.py
@@ -1,5 +1,7 @@
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 iris = datasets.load_iris()
 
 
@@ -22,3 +24,11 @@ print(r5)
 
 #确认模型结构
 print(knn5)
+
+
+y5_pred = knn5.predict(X)
+
+print('k=5准确率是',accuracy_score(y, y5_pred))
+
+y_pred = knn.predict(X)
+print('k=1准确率是',accuracy_score(y, y_pred))
