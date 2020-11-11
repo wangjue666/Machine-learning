37433670a532338408ee6e02b76d1ca949be7a5b
王珏 <wangjuesix@gmail.com>
2020-11-11 10:44:27 +0800

    分离测试集和训练集 分别进行准确率评估

--- sklearn-sd/trainKNN.py
+++ sklearn-sd/trainKNN.py
@@ -1,13 +1,12 @@
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-
+from sklearn.model_selection import train_test_split
 iris = datasets.load_iris()
 
 
 X = iris.data
 y = iris.target
-
 
 #创建实例
 knn = KNeighborsClassifier(n_neighbors=1)
@@ -32,3 +31,18 @@ print('k=5准确率是',accuracy_score(y, y5_pred))
 
 y_pred = knn.predict(X)
 print('k=1准确率是',accuracy_score(y, y_pred))
+
+
+#数据分离
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =0.4)
+
+print('分离后数据维度', X_train.shape)
+
+#分离数据集的训练与评估
+knn_5_s = KNeighborsClassifier(n_neighbors=5)
+knn_5_s.fit(X_train, y_train)
+
+y_train_pred = knn_5_s.predict(X_train)
+y_test_pred = knn_5_s.predict(X_test)
+
+print("分离后数据预测", accuracy_score(y_train, y_train_pred), accuracy_score(y_test, y_test_pred))
